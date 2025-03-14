5773c292860bb7fdb770cee57a157f126040e022
nijutsa <ntjuju82@gmail.com>
2025-03-14 13:28:19 +0200

    first commit

---
+++ src/main/java/gr/jujuras/setlistplaylist/SetlistPlaylistApplication.java
@@ -0,0 +1,13 @@
+package gr.jujuras.setlistplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistPlaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetlistPlaylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/jujuras/setlistplaylist/SetlistPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.jujuras.setlistplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistPlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
