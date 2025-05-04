63b6186c95edd815a53afef68ff0366c87075a8f
nijutsa <ntjuju82@gmail.com>
2025-05-04 14:31:51 +0300

    spring data dependency added

--- src/main/java/gr/jujuras/setlistplaylist/model/User.java
+++ src/main/java/gr/jujuras/setlistplaylist/model/User.java
@@ -6,7 +6,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+
 public class User implements UserDetails {
+
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
