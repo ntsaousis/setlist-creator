6b1aab117c667589e555ca1edb055f719d493633
nijutsa <ntjuju82@gmail.com>
2025-05-06 10:44:17 +0300

    Refactoring RestClient instead of RestTemplate

--- src/main/java/gr/jujuras/setlistplaylist/config/AppConfig.java
+++ src/main/java/gr/jujuras/setlistplaylist/config/AppConfig.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient restClient() {
+        return RestClient.create();
     }
 
 }
+
